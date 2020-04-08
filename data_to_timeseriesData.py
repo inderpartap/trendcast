6938e6a9caf7a9507d7cb18e5c6d8bb5e912dde0
Pallavi Bharadwaj <pallavib@sfu.ca>
2020-04-07 20:19:44 -0700

    loading date column in correct format and generalizing data access

--- data_to_timeseriesData.py
+++ data_to_timeseriesData.py
@@ -3,18 +3,18 @@ from scipy.stats import spearmanr
 from datetime import datetime 
 import numpy as np
 from sklearn.model_selection import TimeSeriesSplit
-from utils.utils import datapath
+from utils.utils import *
 
 
 
 #make sure we have a "DateTime column in the df"
 #creating date features useful in timeseries modeling
 def get_date_features(df):
-    if 'DateTime' in df.columns:
-        df['year'] = df.DateTime.dt.year
-        df['month'] = df.DateTime.dt.month
-        df['day'] = df.DateTime.dt.day
-        df['dayOfWeek'] = df.DateTime.dt.dayofweek
+    if 'date' in df.columns:
+        df['year'] = df.date.dt.year
+        df['month'] = df.date.dt.month
+        df['day'] = df.date.dt.day
+        df['dayOfWeek'] = df.date.dt.dayofweek
 
     return df
 
@@ -30,10 +30,11 @@ def create_weekend_feature(df,col):
 def split_train_test_ts(df,startdate,endDate,no_months):
 
 	#sorting the index to split the data based on dates
-	df = df.sort_index()
+	df = df.set_index('date').sort_index()
 
 	#creating datetime obj
 	datetime_obj = datetime.strptime(endDate, '%Y-%m-%d')
+
 	new_year = datetime_obj.year
 	new_month = (datetime_obj.month - no_months)%12
 	if(no_months >= datetime_obj.month):
@@ -52,24 +53,17 @@ def split_train_test_ts(df,startdate,endDate,no_months):
 
 def main():
 
-	trendcast_data = pd.read_csv(datapath['trendcast']).drop(["Unnamed: 0"], axis=1)
-	#trendcast_data = pd.read_csv("trendcast_dataset.txt",sep=",").drop(["Unnamed: 0"], axis=1)
+	trendcast_data = load_data(datapath['trendcast'], 'csv')
 
-
-	#setting index of the dataframe to date
-	df_dateindexed = trendcast_data.set_index('date')
-	df_dateindexed['DateTime'] = pd.to_datetime(df_dateindexed.index,format="%Y-%m-%d") 
-
-
-	df_dateindexed = get_date_features(df_dateindexed)
-	df_dateindexed = create_weekend_feature(df_dateindexed,"dayOfWeek")
+	trendcast_data = get_date_features(trendcast_data)
+	trendcast_data = create_weekend_feature(trendcast_data,"dayOfWeek")
 
 	#define start date and end date for the train and test split, also with the size of test data based on number of columns
-	no_months_test_data =3		
-	startdate = min(df_dateindexed.index)
-	endDate = max(df_dateindexed.index)
+	no_months_test_data =3
+	startdate = min(trendcast_data.date).strftime('%Y-%m-%d')
+	endDate = max(trendcast_data.date).strftime('%Y-%m-%d')
 
-	X_train,X_test = split_train_test_ts(df_dateindexed,startdate,endDate,no_months_test_data)
+	X_train,X_test = split_train_test_ts(trendcast_data,startdate,endDate,no_months_test_data)
 
 	#saving train and test data
 	X_train.to_csv(datapath['train_data'])
@@ -78,4 +72,3 @@ def main():
 
 if __name__=='__main__':
 	main()
-
