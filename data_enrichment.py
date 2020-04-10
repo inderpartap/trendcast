41afe8a4fae6f6b5435baae4c5d15373a6f1a8b3
Restyled.io <commits@restyled.io>
2020-04-10 09:47:13 +0000

    Restyled by reorder-python-imports

--- data_enrichment.py
+++ data_enrichment.py
@@ -1,8 +1,9 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 
 def transform_date(df):
