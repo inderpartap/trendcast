1c13669101aba70c1d6e9257ffd90f6dc6b95fe0
Restyled.io <commits@restyled.io>
2020-04-11 04:56:27 +0000

    Restyled by black

--- department_modeling.py
+++ department_modeling.py
@@ -12,36 +12,60 @@ import data_to_timeseriesData as data_to_ts
 
 class Department_Modeling:
     def __init__(self):
-        '''
+        """
         Instantiate the prophet model
 
-        '''
+        """
         # holidays and special days
-        playoffs = pd.DataFrame({
-            'holiday': 'playoff',
-            'ds': pd.to_datetime(['2014-11-13', '2015-11-12', '2016-11-10', '2017-11-16', '2018-11-15', '2019-11-14',
-                                  '2014-12-26', '2015-11-27', '2016-12-26']),
-            'lower_window': 0,
-            'upper_window': 0,
-        })
+        playoffs = pd.DataFrame(
+            {
+                "holiday": "playoff",
+                "ds": pd.to_datetime(
+                    [
+                        "2014-11-13",
+                        "2015-11-12",
+                        "2016-11-10",
+                        "2017-11-16",
+                        "2018-11-15",
+                        "2019-11-14",
+                        "2014-12-26",
+                        "2015-11-27",
+                        "2016-12-26",
+                    ]
+                ),
+                "lower_window": 0,
+                "upper_window": 0,
+            }
+        )
         # additional effect on top of the playoffs
         # black friday each year
-        superbowls = pd.DataFrame({
-            'holiday': 'superbowl',
-            'ds': pd.to_datetime(['2014-11-13', '2015-11-12', '2016-11-10', '2017-11-16', '2018-11-15', '2019-11-14']),
-            'lower_window': 0,
-            'upper_window': 0,
-        })
+        superbowls = pd.DataFrame(
+            {
+                "holiday": "superbowl",
+                "ds": pd.to_datetime(
+                    [
+                        "2014-11-13",
+                        "2015-11-12",
+                        "2016-11-10",
+                        "2017-11-16",
+                        "2018-11-15",
+                        "2019-11-14",
+                    ]
+                ),
+                "lower_window": 0,
+                "upper_window": 0,
+            }
+        )
         holidays = pd.concat((playoffs, superbowls))
         self.model = Prophet(daily_seasonality=True, holidays=holidays)
-        self.model.add_country_holidays(country_name='CA')  # Canada holidays
+        self.model.add_country_holidays(country_name="CA")  # Canada holidays
 
     def fit_model(self, df, y, regressors):
-        '''
+        """
             give a list of regressors present in the dataframe that needed to be addeed to make the model
             y should be the target or the response variable
 
-        '''
+        """
 
         for reg in regressors:
             self.model.add_regressor(reg)
@@ -49,43 +73,45 @@ class Department_Modeling:
         # make copy of dataframe and transform it
         df_copy = df.copy()
         df_copy = df_copy.reset_index()
-        df_copy = df_copy.rename({'date': 'ds', y: 'y'}, axis=1)
+        df_copy = df_copy.rename({"date": "ds", y: "y"}, axis=1)
 
         self.fit_model = self.model.fit(df_copy)
         return self
 
     def predict_model_val(self, X, y):
-        '''
+        """
             Predict output for a dataframe X
 
-        '''
+        """
         X_copy = X.copy()
         X_copy = X_copy.reset_index()
-        X_copy = X_copy.rename({'date': 'ds', y: 'y'}, axis=1)
+        X_copy = X_copy.rename({"date": "ds", y: "y"}, axis=1)
         self.forecast_vals = self.fit_model.predict(X_copy)
         return self
 
     def evaluate_model(self, X_orig, y_orig):
-        '''
+        """
         Evaluate model performance
-        '''
+        """
 
-        X_orig = X_orig.reset_index().drop(columns=['date'])
-        print(self.forecast_vals['yhat'])
-        mae = abs(self.forecast_vals['yhat'] - X_orig[y_orig]).mean()
+        X_orig = X_orig.reset_index().drop(columns=["date"])
+        print(self.forecast_vals["yhat"])
+        mae = abs(self.forecast_vals["yhat"] - X_orig[y_orig]).mean()
         return mae
 
 
 def saving_model(model, filename, isWeather):
     print("saving model")
-    if(isWeather):
-        filesavingpath = modelpath['department_models'] + \
-            '/weather/' + filename + '.pckl'
+    if isWeather:
+        filesavingpath = (
+            modelpath["department_models"] + "/weather/" + filename + ".pckl"
+        )
     else:
-        filesavingpath = modelpath['department_models'] + \
-            '/without_weather/' + filename + '.pckl'
+        filesavingpath = (
+            modelpath["department_models"] + "/without_weather/" + filename + ".pckl"
+        )
 
-    with open(filesavingpath, 'wb') as fout:
+    with open(filesavingpath, "wb") as fout:
         pickle.dump(model, fout)
 
     return
@@ -99,47 +125,66 @@ def make_city_dept_models(cities_list, department_list, df, isWeather):
 
     for city in cities_list:
         for dept in department_list:
-            filtered_df = df[(df['city'] == city) & (df['department'] == dept)]
+            filtered_df = df[(df["city"] == city) & (df["department"] == dept)]
             # define a threshold
-            if(len(filtered_df) >= 1000):
-                if (isWeather):
-                    columns_to_drop = ['province', 'city', 'department', 'peakgust', 'pressure',
-                                       'temperature_min', 'temperature_max', 'winddirection']
-                    regressors = ['totalSales', 'temperature']
+            if len(filtered_df) >= 1000:
+                if isWeather:
+                    columns_to_drop = [
+                        "province",
+                        "city",
+                        "department",
+                        "peakgust",
+                        "pressure",
+                        "temperature_min",
+                        "temperature_max",
+                        "winddirection",
+                    ]
+                    regressors = ["totalSales", "temperature"]
                 else:
-                    columns_to_drop = ['province', 'city', 'department', 'peakgust', 'pressure', 'temperature_min',
-                                       'temperature_max', 'winddirection', 'windspeed', 'precipitation', 'temperature']
-                    regressors = ['totalSales']
+                    columns_to_drop = [
+                        "province",
+                        "city",
+                        "department",
+                        "peakgust",
+                        "pressure",
+                        "temperature_min",
+                        "temperature_max",
+                        "winddirection",
+                        "windspeed",
+                        "precipitation",
+                        "temperature",
+                    ]
+                    regressors = ["totalSales"]
                 filtered_df = filtered_df.drop(columns=columns_to_drop)
-                startdate = min(filtered_df.date).strftime('%Y-%m-%d')
-                endDate = max(filtered_df.date).strftime('%Y-%m-%d')
+                startdate = min(filtered_df.date).strftime("%Y-%m-%d")
+                endDate = max(filtered_df.date).strftime("%Y-%m-%d")
                 X_train, X_test = data_to_ts.split_train_test_ts(
-                    filtered_df, startdate, endDate, no_months)
+                    filtered_df, startdate, endDate, no_months
+                )
 
                 model_obj = Department_Modeling()
-                fit_model = model_obj.fit_model(
-                    X_train, 'totalQuantity', regressors)
-                pred_vals = fit_model.predict_model_val(
-                    X_test, 'totalQuantity')
-                mae = pred_vals.evaluate_model(X_test, 'totalQuantity')
+                fit_model = model_obj.fit_model(X_train, "totalQuantity", regressors)
+                pred_vals = fit_model.predict_model_val(X_test, "totalQuantity")
+                mae = pred_vals.evaluate_model(X_test, "totalQuantity")
                 print("MAE for City {} and Dept {} is {}".format(city, dept, mae))
                 # save model
-                saving_model(model_obj, city + "_" +
-                             dept + "_model", isWeather)
+                saving_model(model_obj, city + "_" + dept + "_model", isWeather)
 
 
 def main():
-    department_df = pd.read_csv(datapath['department_level_data'], parse_dates=[
-                                'date'], date_parser=pd.to_datetime)
+    department_df = pd.read_csv(
+        datapath["department_level_data"],
+        parse_dates=["date"],
+        date_parser=pd.to_datetime,
+    )
 
     # get cities
-    cities_list = department_df['city'].unique()
+    cities_list = department_df["city"].unique()
     # get department list
-    department_list = department_df['department'].unique()
+    department_list = department_df["department"].unique()
 
-    make_city_dept_models(cities_list, department_list,
-                          department_df, isWeather=False)
+    make_city_dept_models(cities_list, department_list, department_df, isWeather=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
