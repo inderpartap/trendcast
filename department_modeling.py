c2d167a6e96f6b34b23577764edd75a31b2d7fc3
Restyled.io <commits@restyled.io>
2020-04-11 04:56:33 +0000

    Restyled by yapf

--- department_modeling.py
+++ department_modeling.py
@@ -19,45 +19,45 @@ class Department_Modeling:
 
         """
         # holidays and special days
-        playoffs = pd.DataFrame(
-            {
-                "holiday": "playoff",
-                "ds": pd.to_datetime(
-                    [
-                        "2014-11-13",
-                        "2015-11-12",
-                        "2016-11-10",
-                        "2017-11-16",
-                        "2018-11-15",
-                        "2019-11-14",
-                        "2014-12-26",
-                        "2015-11-27",
-                        "2016-12-26",
-                    ]
-                ),
-                "lower_window": 0,
-                "upper_window": 0,
-            }
-        )
+        playoffs = pd.DataFrame({
+            "holiday":
+            "playoff",
+            "ds":
+            pd.to_datetime([
+                "2014-11-13",
+                "2015-11-12",
+                "2016-11-10",
+                "2017-11-16",
+                "2018-11-15",
+                "2019-11-14",
+                "2014-12-26",
+                "2015-11-27",
+                "2016-12-26",
+            ]),
+            "lower_window":
+            0,
+            "upper_window":
+            0,
+        })
         # additional effect on top of the playoffs
         # black friday each year
-        superbowls = pd.DataFrame(
-            {
-                "holiday": "superbowl",
-                "ds": pd.to_datetime(
-                    [
-                        "2014-11-13",
-                        "2015-11-12",
-                        "2016-11-10",
-                        "2017-11-16",
-                        "2018-11-15",
-                        "2019-11-14",
-                    ]
-                ),
-                "lower_window": 0,
-                "upper_window": 0,
-            }
-        )
+        superbowls = pd.DataFrame({
+            "holiday":
+            "superbowl",
+            "ds":
+            pd.to_datetime([
+                "2014-11-13",
+                "2015-11-12",
+                "2016-11-10",
+                "2017-11-16",
+                "2018-11-15",
+                "2019-11-14",
+            ]),
+            "lower_window":
+            0,
+            "upper_window":
+            0,
+        })
         holidays = pd.concat((playoffs, superbowls))
         self.model = Prophet(daily_seasonality=True, holidays=holidays)
         self.model.add_country_holidays(country_name="CA")  # Canada holidays
@@ -105,13 +105,11 @@ class Department_Modeling:
 def saving_model(model, filename, isWeather):
     print("saving model")
     if isWeather:
-        filesavingpath = (
-            modelpath["department_models"] + "/weather/" + filename + ".pckl"
-        )
+        filesavingpath = (modelpath["department_models"] + "/weather/" +
+                          filename + ".pckl")
     else:
-        filesavingpath = (
-            modelpath["department_models"] + "/without_weather/" + filename + ".pckl"
-        )
+        filesavingpath = (modelpath["department_models"] +
+                          "/without_weather/" + filename + ".pckl")
 
     with open(filesavingpath, "wb") as fout:
         pickle.dump(model, fout)
@@ -161,16 +159,19 @@ def make_city_dept_models(cities_list, department_list, df, isWeather):
                 startdate = min(filtered_df.date).strftime("%Y-%m-%d")
                 endDate = max(filtered_df.date).strftime("%Y-%m-%d")
                 X_train, X_test = data_to_ts.split_train_test_ts(
-                    filtered_df, startdate, endDate, no_months
-                )
+                    filtered_df, startdate, endDate, no_months)
 
                 model_obj = Department_Modeling()
-                fit_model = model_obj.fit_model(X_train, "totalQuantity", regressors)
-                pred_vals = fit_model.predict_model_val(X_test, "totalQuantity")
+                fit_model = model_obj.fit_model(X_train, "totalQuantity",
+                                                regressors)
+                pred_vals = fit_model.predict_model_val(
+                    X_test, "totalQuantity")
                 mae = pred_vals.evaluate_model(X_test, "totalQuantity")
-                print("MAE for City {} and Dept {} is {}".format(city, dept, mae))
+                print("MAE for City {} and Dept {} is {}".format(
+                    city, dept, mae))
                 # save model
-                saving_model(model_obj, city + "_" + dept + "_model", isWeather)
+                saving_model(model_obj, city + "_" + dept + "_model",
+                             isWeather)
 
 
 def main():
@@ -185,7 +186,10 @@ def main():
     # get department list
     department_list = department_df["department"].unique()
 
-    make_city_dept_models(cities_list, department_list, department_df, isWeather=False)
+    make_city_dept_models(cities_list,
+                          department_list,
+                          department_df,
+                          isWeather=False)
 
 
 if __name__ == "__main__":
