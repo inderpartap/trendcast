b0ce1bc3fd1368a9896ec874f716ff076b50c931
Restyled.io <commits@restyled.io>
2020-04-14 09:24:05 +0000

    Restyled by autopep8

--- app/app.py
+++ app/app.py
@@ -5,42 +5,45 @@ from utilities import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/sales')
 def index():
-    return render_template("base1.html", message="Static Sales Dashboard");   
+    return render_template("base1.html", message="Static Sales Dashboard")
+
 
 @app.route('/predictions')
 def index2():
-    return render_template("base2.html", message="Predictions Dashboard");   
+    return render_template("base2.html", message="Predictions Dashboard")
 
-@app.route('/sales/result',methods = ['POST', 'GET'])
+
+@app.route('/sales/result', methods=['POST', 'GET'])
 def sales_data():
-   if request.method == 'POST':
-      cityname = request.form.getlist('city')[0]
-      data = static_data(cityname)
-      return render_template("sales.html",result = data, city = cityname)
+    if request.method == 'POST':
+        cityname = request.form.getlist('city')[0]
+        data = static_data(cityname)
+        return render_template("sales.html", result=data, city=cityname)
 
-@app.route('/predictions/result',methods = ['POST', 'GET'])
+
+@app.route('/predictions/result', methods=['POST', 'GET'])
 def sales_predictions():
-   if request.method == 'POST':
-      cityname = request.form.getlist('city')[0]
-      department = request.form.getlist('department')[0]
-      department = str.lower(department.replace(" ", ""))
-      if department == 'alldepartments':
-        data_base, data_weather  = citylevel(cityname)
-        return render_template("city_predictions.html", result_base = data_base, result_weather = data_weather, city = cityname)
-      else:
-        data_base, data_weather = deptlevel(cityname, department)
-        return render_template("dept_predictions.html",result = data, city = cityname, department = department)
-      
+    if request.method == 'POST':
+        cityname = request.form.getlist('city')[0]
+        department = request.form.getlist('department')[0]
+        department = str.lower(department.replace(" ", ""))
+        if department == 'alldepartments':
+            data_base, data_weather = citylevel(cityname)
+            return render_template("city_predictions.html", result_base=data_base, result_weather=data_weather, city=cityname)
+        else:
+            data_base, data_weather = deptlevel(cityname, department)
+            return render_template("dept_predictions.html", result=data, city=cityname, department=department)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return render_template('404.html', title='Page not found'), 404
+    return render_template('404.html', title='Page not found'), 404
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(host='0.0.0.0' , port=5000)
+    app.run(host='0.0.0.0', port=5000)
--- app/utilities.py
+++ app/utilities.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import json
-import pickle 
+import pickle
 import requests
 
 from utils.utils import *
@@ -11,92 +11,99 @@ from fbprophet import Prophet
 from fbprophet.plot import add_changepoints_to_plot, plot_yearly
 
 
-#serializing json data for compatibility with frontend
+# serializing json data for compatibility with frontend
 class JSONEncoder(json.JSONEncoder):
-	def default(self, obj):
-		if hasattr(obj,'to_dict'):
-			return obj.to_dict()
-		return json.JSONEncoder.default(self, obj)
+    def default(self, obj):
+        if hasattr(obj, 'to_dict'):
+            return obj.to_dict()
+        return json.JSONEncoder.default(self, obj)
+
 
 def load_data(city):
-	city = str.lower(city)
-	df = pd.read_csv("../data/trendcast_dataset.csv")
-	df = df[df.city == city]
-	df = df.sort_values('date')
-	return df
+    city = str.lower(city)
+    df = pd.read_csv("../data/trendcast_dataset.csv")
+    df = df[df.city == city]
+    df = df.sort_values('date')
+    return df
+
 
 def static_data(cityname):
-	df = load_data(cityname)
-	df = df[['date', 'city', 'totalQuantity', 'totalSales']]
-	ser = json.dumps(df, cls=JSONEncoder)
-	unser=json.loads(ser)
-	return unser
+    df = load_data(cityname)
+    df = df[['date', 'city', 'totalQuantity', 'totalSales']]
+    ser = json.dumps(df, cls=JSONEncoder)
+    unser = json.loads(ser)
+    return unser
+
 
 def get_weather(dates, cityname):
-	df = pd.read_csv("../data/city_id_names.csv")
-	df = df[df['name'] == str.lower(cityname)]
-	city_id = df['station_id'].iat[-1]
+    df = pd.read_csv("../data/city_id_names.csv")
+    df = df[df['name'] == str.lower(cityname)]
+    city_id = df['station_id'].iat[-1]
 
-	str_date = dates['ds'].apply(lambda x: x.strftime('%Y.%m.%d'))
-	start_date = str_date.head(1)
-	end_date = str_date.tail(1)
+    str_date = dates['ds'].apply(lambda x: x.strftime('%Y.%m.%d'))
+    start_date = str_date.head(1)
+    end_date = str_date.tail(1)
 
-	response = send_request(
-				api="weather",
-				end_point="daily",
-				params=dict(station=int(city_id),
-							start=start_date,
-							end=end_date),
-			)
-	response_df = pd.DataFrame(response)
-	dates['temperature'] = response_df[['temperature_min', 'temperature_max']].mean(axis=1)
-	return dates
-	
+    response = send_request(
+        api="weather",
+        end_point="daily",
+        params=dict(station=int(city_id),
+                    start=start_date,
+                    end=end_date),
+    )
+    response_df = pd.DataFrame(response)
+    dates['temperature'] = response_df[[
+        'temperature_min', 'temperature_max']].mean(axis=1)
+    return dates
+
+
 def citylevel(cityname):
-	city_file = str.lower(cityname.replace(" ", "_"))
-	path_without_weather = "../models/sales/without_weather/"+city_file+".pkl"
-	path_with_weather = "../models/sales/weather/"+city_file+".pkl"
+    city_file = str.lower(cityname.replace(" ", "_"))
+    path_without_weather = "../models/sales/without_weather/"+city_file+".pkl"
+    path_with_weather = "../models/sales/weather/"+city_file+".pkl"
 
-	df = load_data(cityname)
-	df['y'] = 0
-	df = df.rename(columns={"date": "ds"})
-	temp = df[['ds', 'y']].tail(2)
-	
-	m = Prophet()
-	m.fit(temp)
+    df = load_data(cityname)
+    df['y'] = 0
+    df = df.rename(columns={"date": "ds"})
+    temp = df[['ds', 'y']].tail(2)
 
-	dates = m.make_future_dataframe(periods=7)
-	dates = dates.drop([0, 1]).reset_index(drop=True)
+    m = Prophet()
+    m.fit(temp)
 
-	data = get_weather(dates, cityname)
+    dates = m.make_future_dataframe(periods=7)
+    dates = dates.drop([0, 1]).reset_index(drop=True)
 
-	dates_only = pd.DataFrame(data['ds'])
-	wo_weather_model = Prophet()
-	wo_weather_model = pickle.load(open(path_without_weather, 'rb'))
-	predictions_base = wo_weather_model.predict(dates_only)
+    data = get_weather(dates, cityname)
 
-	output = predictions_base[['ds', 'yhat']]
-	output['yhat'] = np.exp(output['yhat'])
-	output['ds'] = output['ds'].apply(lambda x: x.strftime('%Y.%m.%d'))
-	ser = json.dumps(output, cls=JSONEncoder)
-	unser_base=json.loads(ser)
+    dates_only = pd.DataFrame(data['ds'])
+    wo_weather_model = Prophet()
+    wo_weather_model = pickle.load(open(path_without_weather, 'rb'))
+    predictions_base = wo_weather_model.predict(dates_only)
 
+    output = predictions_base[['ds', 'yhat']]
+    output['yhat'] = np.exp(output['yhat'])
+    output['ds'] = output['ds'].apply(lambda x: x.strftime('%Y.%m.%d'))
+    ser = json.dumps(output, cls=JSONEncoder)
+    unser_base = json.loads(ser)
 
-	w_weather_model = Prophet()
-	w_weather_model = pickle.load(open(path_with_weather, 'rb'))
-	predictions_weather = w_weather_model.predict(data)
+    w_weather_model = Prophet()
+    w_weather_model = pickle.load(open(path_with_weather, 'rb'))
+    predictions_weather = w_weather_model.predict(data)
 
-	output = predictions_weather[['ds', 'yhat']]
-	output['ds'] = output['ds'].apply(lambda x: x.strftime('%Y.%m.%d'))
-	output['yhat'] = np.exp(output['yhat'])
-	ser = json.dumps(output, cls=JSONEncoder)
-	unser_weather = json.loads(ser)
+    output = predictions_weather[['ds', 'yhat']]
+    output['ds'] = output['ds'].apply(lambda x: x.strftime('%Y.%m.%d'))
+    output['yhat'] = np.exp(output['yhat'])
+    ser = json.dumps(output, cls=JSONEncoder)
+    unser_weather = json.loads(ser)
 
-	return unser_base, unser_weather
+    return unser_base, unser_weather
+
 
 def deptlevel(cityname, department):
-	df = load_data(cityname)
+    df = load_data(cityname)
 
-	city_file = str.lower(cityname)
-	path_without_weather = "../models/department_level/without_weather/"+cityname+'_'+department+"_model.pkl"
-	path_with_weather = "../models/department_level/weather/"+cityname+'_'+department+"_model.pkl"
+    city_file = str.lower(cityname)
+    path_without_weather = "../models/department_level/without_weather/" + \
+        cityname+'_'+department+"_model.pkl"
+    path_with_weather = "../models/department_level/weather/" + \
+        cityname+'_'+department+"_model.pkl"
