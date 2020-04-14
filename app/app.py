80a3c57b3d5e3c53421a0fb6032d5379f93becb0
Inderpartap <inderpartaps.cheema2014@vit.ac.in>
2020-04-14 02:19:48 -0700

    Created city level dashboard

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify, request
-import util as ut
+from flask import Flask, render_template, jsonify, request, redirect
+from utilities import *
 # import dash
 # import dash_html_components as html
 
@@ -15,11 +15,25 @@ def index2():
     return render_template("base2.html", message="Predictions Dashboard");   
 
 @app.route('/sales/result',methods = ['POST', 'GET'])
-def result():
+def sales_data():
    if request.method == 'POST':
       cityname = request.form.getlist('city')[0]
-      data = ut.read_data(cityname)
+      data = static_data(cityname)
       return render_template("sales.html",result = data, city = cityname)
+
+@app.route('/predictions/result',methods = ['POST', 'GET'])
+def sales_predictions():
+   if request.method == 'POST':
+      cityname = request.form.getlist('city')[0]
+      department = request.form.getlist('department')[0]
+      department = str.lower(department.replace(" ", ""))
+      if department == 'alldepartments':
+        data_base, data_weather  = citylevel(cityname)
+        return render_template("city_predictions.html", result_base = data_base, result_weather = data_weather, city = cityname)
+      else:
+        data_base, data_weather = deptlevel(cityname, department)
+        return render_template("dept_predictions.html",result = data, city = cityname, department = department)
+      
 
 
 @app.errorhandler(404)
