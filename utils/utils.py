b204b1afa4dc334971ac0c8f71f4e31e74c145f3
Restyled.io <commits@restyled.io>
2020-04-17 05:12:06 +0000

    Restyled by yapf

--- utils/utils.py
+++ utils/utils.py
@@ -129,7 +129,8 @@ def send_request(api, end_point, params):
     # check if all required parameters are passed
     for key, val in json[end_point]["params"].items():
         if val == True and key not in params:
-            print('ERROR: "%s" is a required parameter for this request' % (key))
+            print('ERROR: "%s" is a required parameter for this request' %
+                  (key))
             return dict()
 
     # add auth key to parameters
